f75b94fd3d45f1f702367ce92c7b901bd624e15d
midoricoding <francine.midori@gmail.com>
2021-10-28 14:59:55 +0200

    init_git

--- welcome.py
+++ welcome.py
@@ -3,9 +3,12 @@ from abteil_1 import abteil_1
 
 winning_streak = []
 
+
 def streak_b():
-    final_action=input("""Die beiden verlassen den Bahnhof. Du verpasst deinen Zug und dein
-    toller Gewinn verfällt... Willst du noch einmal von vorn beginnen? (Y)es or (N)o""")
+    final_action = input(
+        """Die beiden verlassen den Bahnhof. Du verpasst deinen Zug und dein
+    toller Gewinn verfällt... Willst du noch einmal von vorn beginnen? (Y)es or (N)o"""
+    )
     print()
     abteil_1.inventar.clear()
     if final_action.upper() == "Y":
@@ -14,17 +17,23 @@ def streak_b():
         print()
         print("Schade! Auf Wiedersehen!")
 
+
 def welcome():
-    urlaubsgeld= random.randrange(90, 110)
+    urlaubsgeld = random.randrange(90, 110)
     abteil_1.assign_inventar(urlaubsgeld)
     username = input("Gib deinen Spieler*innennamen an?")
     print()
-    print(f"""Herzlichen Glückwunsch!\n{username} du hast eine Zugfahrt im Bergland-Express im Radio-Quiz gewonnen! Du hast {urlaubsgeld}DM Urlaubsgeld dabei!\nDu beginnst deine Reise am Bahnsteig, wo dir ein seltsames Paar auffällt.""")
-    action_1=input("""Was tust du? (B)eobachten oder (I)gnorieren""")
+    print(
+        f"""Herzlichen Glückwunsch!\n{username} du hast eine Zugfahrt im Bergland-Express im Radio-Quiz gewonnen! Du hast {urlaubsgeld}DM Urlaubsgeld dabei!\nDu beginnst deine Reise am Bahnsteig, wo dir ein seltsames Paar auffällt."""
+    )
+    action_1 = input("""Was tust du? (B)eobachten oder (I)gnorieren""")
     print()
     if action_1.upper() == "B":
         streak_b()
     elif action_1.upper() == "I":
         abteil_1.init_streak(1)
 
+
 welcome()
+
+# Hallo Katrin!!!
