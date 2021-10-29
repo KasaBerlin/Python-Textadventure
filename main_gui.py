1a6464b0a16a3a024329153d5722bddaf6c5369a
midoricoding <francine.midori@gmail.com>
2021-10-29 16:47:31 +0200

    clean up

--- main_gui.py
+++ main_gui.py
@@ -3,20 +3,24 @@ from tkinter import *
 # tkinter-window mit Titel und Größe erzeugen
 app = Tk()
 app.title("Reise im Bergland-Express")
-app.geometry("800x600+200+200") # Breite 400, Höhe 300, x und y -Koordinate auf 300 und 200 setzen
+app.geometry(
+    "800x600+200+200"
+)  # Breite 400, Höhe 300, x und y -Koordinate auf 300 und 200 setzen
+
+label_current = Label(app, text="Bitte gibt einen Usernamen ein")
+label_current.pack(pady=10)
 
 text_username = Entry(app)
 text_username.pack()
 
-label_current = Label(app,text="Bitte gibt einen Usernamen ein")
-label_current.pack(pady=10)
+button_current_1 = Button(app)
+button_current_2 = Button(app)
 
-button_current_1=Button(app)
-button_current_2=Button(app)
 
-def reassign_button(button,text,command):
-    button["text"]=text
-    button["command"]=command
-    button.pack(fill='x')
+def reassign_button(button, text, command):
+    button["text"] = text
+    button["command"] = command
+    button.pack(fill="x")
+
 
 # ! mainloop befindet sich in welcome_gui
