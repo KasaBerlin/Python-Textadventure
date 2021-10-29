f9fc41e90c5722a1543ae9431b2c60ec46bd90c8
midoricoding <francine.midori@gmail.com>
2021-10-29 11:45:51 +0200

    layout

---
+++ layout.py
@@ -0,0 +1,26 @@
+app = {
+    "width": 900,
+    "height": 500,
+}
+
+fonts = {
+    "font_welcome_gratulations": {
+        "family": "Playfair Display",
+        "size": 20,
+        "weight": "normal",
+    },
+    "font_texts": {"family": "Didot", "size": 18, "weight": "normal"},
+    "font_buttons": {"family": "Helvetica", "size": 16, "weight": "bold"},
+}
+
+background = {
+    "bg_welcome": "#4287f5",
+    "bg_button1_welcome": "#4287f5",
+    "bg_button2_welcome": "#4287f5",
+    "bg_abteil1": "#4287f5",
+    "bg_button1_abteil1": "#4287f5",
+    "bg_button2_abteil1": "#4287f5",
+    "bg_abteil2": "#4287f5",
+    "bg_button1_abteil2": "#4287f5",
+    "bg_button2_abteil2": "#4287f5",
+}
--- welcome_gui.py
+++ welcome_gui.py
@@ -1,21 +1,21 @@
 from tkinter import *
 import random
 from abteil_1 import abteil_1
+from tkinter import font
+import layout
+
 
 # tkinter-window mit Titel und Größe erzeugen
 app = Tk()
 app.title("Reise im Bergland-Express")
-app.geometry("800x600+200+200")
-frame = Frame(bd=3, relief="raised")
+app.geometry("900x600+200+200")
 
-label_current = Label(frame, text="Bitte gibt einen Usernamen ein")
+font_ex = font.Font(family="Aller", size=26, weight="normal")
+
+label_current = Label(text="Bitte gibt einen Usernamen ein")
 label_current.pack(pady=10)
 
-text_username = Entry(frame)
-text_username.pack()
-text_username.bind('<Return>', welcome)
-
-current_entry=Entry(frame)
+current_entry = Entry()
 
 
 # def streak_b():
@@ -31,24 +31,31 @@ current_entry=Entry(frame)
 #         print()
 #         print("Schade! Auf Wiedersehen!")
 
+
 def welcome(e):
     urlaubsgeld = random.randrange(90, 110)
     abteil_1.assign_inventar(urlaubsgeld)
-    text1=f"""Herzlichen Glückwunsch!\n{text_username.get()} du hast eine Zugfahrt im Bergland-Express im Radio-Quiz 
+    text1 = f"""Herzlichen Glückwunsch!\n{text_username.get()} du hast eine Zugfahrt im Bergland-Express im Radio-Quiz 
       gewonnen! Du hast {urlaubsgeld}DM Urlaubsgeld dabei!\nDu beginnst deine Reise am Bahnsteig, wo dir ein seltsames 
-      Paar auffällt. Was tust du?"""
-    text_username.forget_pack()
+      Paar auffällt.\n Was tust du?"""
+    text_username.pack_forget()
+
+    label_current.configure(font=font_ex)
     label_current.config(text=text1)
-    button_beobachten = Button(frame,text="Beobachten")
+
+    button_beobachten = Button(text="Beobachten")
     button_beobachten.pack()
-    button_ignorieren = Button(frame,text="Ignorieren")
+    button_ignorieren = Button(text="Ignorieren")
     button_ignorieren.pack()
     # if action_1.upper() == "B":
     #     streak_b()
     # elif action_1.upper() == "I":
     #     abteil_1.init_streak(1)
 
-welcome()
+
+text_username = Entry()
+text_username.pack()
+text_username.bind("<Return>", welcome)
 
 """
 buttonX = tk.Button(app, text="Fill X", bg="red", height=5)
@@ -59,5 +66,4 @@ buttonY.pack(side='left', fill='y')
 """
 
 
-frame.pack(pady=10)
 app.mainloop()
