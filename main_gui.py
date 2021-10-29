eb8a4f40a691f88998190ba5fbe49eaada40283a
KasaBerlin <katrinsalac@gmail.com>
2021-10-29 16:46:09 +0200

    clean_up

--- main_gui.py
+++ main_gui.py
@@ -5,11 +5,11 @@ app = Tk()
 app.title("Reise im Bergland-Express")
 app.geometry("800x600+200+200") # Breite 400, Höhe 300, x und y -Koordinate auf 300 und 200 setzen
 
-text_username = Entry(app)
-text_username.pack()
-
 label_current = Label(app,text="Bitte gibt einen Usernamen ein")
 label_current.pack(pady=10)
+
+text_username = Entry(app)
+text_username.pack()
 
 button_current_1=Button(app)
 button_current_2=Button(app)
