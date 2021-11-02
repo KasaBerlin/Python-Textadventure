4a8b21e9cb58b794266b8faf33b4107debfeb412
KasaBerlin <katrinsalac@gmail.com>
2021-11-02 19:59:13 +0100

    abteil_2_tkinter

--- abteil_2.py
+++ abteil_2.py
@@ -1,32 +1,38 @@
 import abteil_constructor
+from main_gui import *
+from tkinter import ttk
 
 def streak_1():
     speisekarte = {
         "Getränke": {
-            "(W)hiskey 4cl": 8,
-            "(B)ier 0.5ml": 4,
-            "(M)ilch 300ml": 3,
-            "Tasse (KA)ffee": 5,
-            "Tasse (T)ee-verschiedene Sorten": 4,
+            "Whiskey 4cl": 8,
+            "Bier 0.5ml": 4,
+            "Milch 300ml": 3,
+            "Tasse Kaffee": 5,
+            "Tasse Tee-verschiedene Sorten": 4,
         },
-        "Speisen": {"(KU)chen des Tages": 10, "(E)isbein": 15, "(S)uppe des Tages": 10},
+        "Speisen": {"Kuchen des Tages": 10, "Eisbein": 15, "Suppe des Tages": 10},
     }
-    print(speisekarte)
-    auswahl = input("Wähle aus!").upper()
-    for item in speisekarte:
-        for i in speisekarte[item].keys():
-            if i.find(f"({auswahl})") != -1:
-                abteil_constructor.abteil_2_obj.inventar[0] -= speisekarte[item][i]
-                print(f"Du hast jetzt noch {abteil_constructor.abteil_2_obj.inventar[0]} DM im Inventar.")
-                if (
-                    input(
-                        "Der Kellner fragt dich, ob du noch etwas bestellen möchtest? (J)a oder (N)ein"
-                    ).upper()
-                    == "J"
-                ):
-                    streak_1()
-                else:
-                    print(
-                        "Du kehst zu deinem Platz zurück. Die Alte Lady mit dem Mops lächelt dich nett an und fragt dich, ob du sein wahres Alter erraten kannst."
-                    )
-                    abteil_constructor.abteil_1_obj.dog_quiz()
+    getraenke= ttk.Combobox(app, values = list(speisekarte["Getränke"].keys()))
+    getraenke.set("wähle aus")
+    getraenke.pack()
+    speisen=ttk.Combobox(app, values = list(speisekarte["Speisen"].keys()))
+    speisen.set("wähle aus")
+    speisen.pack()
+    # for item in speisekarte:
+    #     for i in speisekarte[item].keys():
+    #         if i.find(f"({auswahl})") != -1:
+    #             abteil_constructor.abteil_2_obj.inventar[0] -= speisekarte[item][i]
+    #             print(f"Du hast jetzt noch {abteil_constructor.abteil_2_obj.inventar[0]} DM im Inventar.")
+    #             if (
+    #                 input(
+    #                     "Der Kellner fragt dich, ob du noch etwas bestellen möchtest? (J)a oder (N)ein"
+    #                 ).upper()
+    #                 == "J"
+    #             ):
+    #                 streak_1()
+    #             else:
+    #                 print(
+    #                     "Du kehst zu deinem Platz zurück. Die Alte Lady mit dem Mops lächelt dich nett an und fragt dich, ob du sein wahres Alter erraten kannst."
+    #                 )
+    #                 abteil_constructor.abteil_1_obj.dog_quiz()
